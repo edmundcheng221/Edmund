7f666e931088e386d180c83e8a475c22dd5f9a7d
Edmund Cheng <61927300+edmundcheng221@users.noreply.github.com>
2020-05-22 14:45:34 -0400

    Update gpa

--- cv.js
+++ cv.js
@@ -16,7 +16,7 @@ function data() {
                 college: {
                     institution: "New York University",
                     major: "B.S., Mechanical Engineering, Integrated Digital Media Minor (2021)",
-                    honors: ["GPA: 3.06, NYU Tandon Scholarship, NYU Trio Scholar"],
+                    honors: ["Major GPA: 3.32, NYU Tandon Scholarship, NYU Trio Scholar"],
                     accolades: "International Baccalaureat Diploma, NYC Comptroller’s Student Achievement Award (2017), Society for the Advancement of Material Processes and Engineering, American Society of Mechanical Engineers (NYU Chapter)" 
                     /*notableCourses: [
                         "SolidWorks 3D Modeling",
